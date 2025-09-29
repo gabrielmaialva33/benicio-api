dec48fd78692468e43040ddaa5e740f01771e092
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-09-29 14:28:31 -0300

    chore: update CORS config to allow all origins and additional HTTP methods

--- config/cors.ts
+++ config/cors.ts
@@ -8,8 +8,8 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: [],
-  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
+  origin: '*',
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   headers: true,
   exposeHeaders: [],
   credentials: true,
